ee192d6fb3f48d2cd60582d2fda36725662de895
ry <ry@example.com>
2025-06-30 22:55:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ background.addEventListener('click', function() {
     body.classList.remove('navigation_open');
     body.style.overflow = 'auto';
 });
+
+//scroll to top
+const arrowUpward = document.getElementById('arrow_upward');
+function scrollUpward() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+arrowUpward.addEventListener('click', scrollUpward);
