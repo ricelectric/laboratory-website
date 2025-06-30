b70f5ad8507313ee1c6b7eac619286b38b04a1a9
ry <ry@example.com>
2025-06-30 22:51:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,35 +16,3 @@ background.addEventListener('click', function() {
     body.classList.remove('navigation_open');
     body.style.overflow = 'auto';
 });
-
-//scroll effect
-// let triggerMargin = 100;
-
-// const scrollAnimationElements = document.querySelectorAll('.scroll_effect');
-// const scrollAnimationFunction = function () {
-//     for (var i = 0; i < scrollAnimationElements.length; i++) {
-//         if (window.innerHeight - triggerMargin > scrollAnimationElements[i].getBoundingClientRect().top) {
-//             scrollAnimationElements[i].classList.add('appear');
-//         }
-//     }
-// }
-// window.addEventListener('load', scrollAnimationFunction);
-// window.addEventListener('scroll', scrollAnimationFunction);
-
-//slider
-// const slides = document.querySelectorAll('.slide');
-// const dots = document.querySelectorAll('.dot');
-
-// let currentIndex = 0;
-// function showSlides() {
-//     slides[currentIndex].classList.remove('active');
-//     dots[currentIndex].classList.remove('active');
-
-//     currentIndex = (currentIndex + 1) % slides.length;
-
-//     slides[currentIndex].classList.add('active');
-//     dots[currentIndex].classList.add('active');
-// }
-// setInterval(showSlides, 10000);
-
-// window.addEventListener('load', showSlides);
