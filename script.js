cbc96aa4500cc46b7cbe86fbf0956425ca348123
ry <ry@example.com>
2025-06-30 23:08:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ background.addEventListener('click', function() {
     body.classList.remove('navigation_open');
     body.style.overflow = 'auto';
 });
+
+//scroll effect
+const options = {
+    threshold: 0.1
+}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('appear');
+        }
+    });
+}, options);
+
+document.querySelectorAll('.scroll_effect').forEach(element => observer.observe(element));
+
+//another version
+// let triggerMargin = 100;
+
+// const scrollAnimationElements = document.querySelectorAll('.scroll_effect');
+// const scrollAnimationFunction = function () {
+//     for (var i = 0; i < scrollAnimationElements.length; i++) {
+//         if (window.innerHeight - triggerMargin > scrollAnimationElements[i].getBoundingClientRect().top) {
+//             scrollAnimationElements[i].classList.add('appear');
+//         }
+//     }
+// }
+// window.addEventListener('load', scrollAnimationFunction);
+// window.addEventListener('scroll', scrollAnimationFunction);
